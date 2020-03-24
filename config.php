8c38f0e2df2369f4318b780019d60f217c0cbb56
shadowoflight <55912551+shadowoflight@users.noreply.github.com>
2020-03-24 23:50:16 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=sanjana@123");
+
+
+  //  if($dbconn)
+    //{
+      //  echo "Connected to the  database successfully";
+    //}
+
+?>
